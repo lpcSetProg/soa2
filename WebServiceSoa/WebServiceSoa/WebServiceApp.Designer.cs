b9d6766275cbbef3ba010d47343fe34296979e31
Leveson Cocarell <Lcocarell2816@conestogac.on.ca>
2018-09-24 16:34:02 -0400

    set up win form added combo box for web services drop down

---
+++ WebServiceSoa/WebServiceSoa/WebServiceApp.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace WebServiceSoa
+{
+    partial class WebServiceApp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox_webServiceSelector = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // comboBox_webServiceSelector
+            // 
+            this.comboBox_webServiceSelector.FormattingEnabled = true;
+            this.comboBox_webServiceSelector.Location = new System.Drawing.Point(39, 55);
+            this.comboBox_webServiceSelector.Name = "comboBox_webServiceSelector";
+            this.comboBox_webServiceSelector.Size = new System.Drawing.Size(196, 21);
+            this.comboBox_webServiceSelector.TabIndex = 0;
+            this.comboBox_webServiceSelector.SelectedIndexChanged += new System.EventHandler(this.comboBox_webServiceSelector_SelectedIndexChanged);
+            // 
+            // WebServiceApp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.comboBox_webServiceSelector);
+            this.Name = "WebServiceApp";
+            this.Text = "Web Services ";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox_webServiceSelector;
+    }
+}
+
---
+++ WebServiceSoa/WebServiceSoa/WebServiceApp.cs
@@ -0,0 +1,43 @@
+﻿/*
+* FILE : SOA_Assignment_02.cs
+* PROJECT : PROG3080 - Service Oriented Architecture - Assignment 2
+* PROGRAMMERS : Lev Cocarell and Bobby Vu 
+* FIRST VERSION : 2018-24-12
+* DESCRIPTION :
+* 
+* 
+*/
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WebServiceSoa
+{
+    public partial class WebServiceApp : Form
+    {
+        public WebServiceApp()
+        {
+            InitializeComponent();
+        }
+
+
+        /*
+        * Method     : comboBox_webServiceSelector_SelectedIndexChanged()
+        * Description: 
+        * Parameters:
+        * Returns:
+        */
+        private void comboBox_webServiceSelector_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
